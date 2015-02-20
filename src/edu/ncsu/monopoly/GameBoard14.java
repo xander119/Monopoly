f5e22ca03836df070c963424a7f49d64ac422e15
xander119 <alexwang119@yahoo.com>
2015-02-20 10:35:47 +0000

    2, Extract Method

--- src/edu/ncsu/monopoly/GameBoard14.java
+++ src/edu/ncsu/monopoly/GameBoard14.java
@@ -1,50 +1,70 @@
 package edu.ncsu.monopoly;
 
-
 public class GameBoard14 extends GameBoard {
+	PropertyCell blue1, blue2, blue3, green1, green2, green3, red1, red2, red3,
+			purple1, purple2, yellow1, yellow2;
+
 	public GameBoard14() {
 		super();
-		PropertyCell blue1 = new PropertyCell();
-		PropertyCell blue2 = new PropertyCell();
-		PropertyCell blue3 = new PropertyCell();
-		PropertyCell green1 = new PropertyCell();
-		PropertyCell green2 = new PropertyCell();
-		PropertyCell green3 = new PropertyCell();
-		PropertyCell red1 = new PropertyCell();
-		PropertyCell red2 = new PropertyCell();
-		PropertyCell red3 = new PropertyCell();
-		PropertyCell purple1 = new PropertyCell();
-		PropertyCell purple2 = new PropertyCell();
-		PropertyCell yellow1 = new PropertyCell();
-		PropertyCell yellow2 = new PropertyCell();
-		
+		initPropertyCells();
+
+		addCell(blue1);
+		addCell(blue2);
+		addCell(blue3);
+		addCell(green1);
+		addCell(green2);
+		addCell(green3);
+		addCell(red1);
+		addCell(red2);
+		addCell(red3);
+		addCell(purple1);
+		addCell(purple2);
+		addCell(yellow1);
+		addCell(yellow2);
+	}
+
+	private void initPropertyCells() {
+		blue1 = new PropertyCell();
+		blue2 = new PropertyCell();
+		blue3 = new PropertyCell();
+		green1 = new PropertyCell();
+		green2 = new PropertyCell();
+		green3 = new PropertyCell();
+		red1 = new PropertyCell();
+		red2 = new PropertyCell();
+		red3 = new PropertyCell();
+		purple1 = new PropertyCell();
+		purple2 = new PropertyCell();
+		yellow1 = new PropertyCell();
+		yellow2 = new PropertyCell();
+
 		blue1.setName("Blue 1");
-        blue1.setColorGroup("blue");
+		blue1.setColorGroup("blue");
 		blue2.setName("Blue 2");
-        blue2.setColorGroup("blue");
+		blue2.setColorGroup("blue");
 		blue3.setName("Blue 3");
-        blue3.setColorGroup("blue");
+		blue3.setColorGroup("blue");
 		green1.setName("Green 1");
-        green1.setColorGroup("green");
+		green1.setColorGroup("green");
 		green2.setName("Green 2");
-        green2.setColorGroup("green");
+		green2.setColorGroup("green");
 		green3.setName("Green 3");
-        green3.setColorGroup("green");
+		green3.setColorGroup("green");
 		red1.setName("Red 1");
-        red1.setColorGroup("red");
+		red1.setColorGroup("red");
 		red2.setName("Red 2");
-        red2.setColorGroup("red");
+		red2.setColorGroup("red");
 		red3.setName("Red 3");
-        red3.setColorGroup("red");
+		red3.setColorGroup("red");
 		purple1.setName("Purple 1");
-        purple1.setColorGroup("purple");
-		purple2.setName("Purple 2");		
-        purple2.setColorGroup("purple");
+		purple1.setColorGroup("purple");
+		purple2.setName("Purple 2");
+		purple2.setColorGroup("purple");
 		yellow1.setName("Yellow 1");
-        yellow1.setColorGroup("yellow");
+		yellow1.setColorGroup("yellow");
 		yellow2.setName("Yellow 2");
-        yellow2.setColorGroup("yellow");
-		
+		yellow2.setColorGroup("yellow");
+
 		blue1.setPrice(100);
 		blue2.setPrice(100);
 		blue3.setPrice(120);
@@ -58,7 +78,6 @@ public class GameBoard14 extends GameBoard {
 		purple2.setPrice(360);
 		yellow1.setPrice(400);
 		yellow2.setPrice(420);
-		
 
 		blue1.setRent(10);
 		blue2.setRent(10);
@@ -73,7 +92,7 @@ public class GameBoard14 extends GameBoard {
 		purple2.setRent(36);
 		yellow1.setRent(40);
 		yellow2.setRent(42);
-		
+
 		blue1.setHousePrice(30);
 		blue2.setHousePrice(30);
 		blue3.setHousePrice(30);
@@ -87,19 +106,5 @@ public class GameBoard14 extends GameBoard {
 		purple2.setHousePrice(60);
 		yellow1.setHousePrice(70);
 		yellow2.setHousePrice(70);
-		
-		addCell(blue1);
-		addCell(blue2);
-		addCell(blue3);
-		addCell(green1);
-		addCell(green2);
-		addCell(green3);
-		addCell(red1);
-		addCell(red2);
-		addCell(red3);
-		addCell(purple1);
-		addCell(purple2);
-		addCell(yellow1);
-		addCell(yellow2);
 	}
 }
